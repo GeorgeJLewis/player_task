3535519b3219f9cc2a86e7a549f68185740ee084
George_Lewis <georgejlewis@hotmail.com>
2018-07-24 11:30:45 +0200

    add babel core and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   }
 };
+
+module: {
+rules: [
+  {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  }
+]
+}
