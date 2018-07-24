2563a4246f7fd275a177b2a5f3aaf4a80b230bb2
George_Lewis <georgejlewis@hotmail.com>
2018-07-24 11:13:00 +0200

    add webpack config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import _ from 'lodash';
 
-function component() {
-  var element = document.createElement('div');
+document.addEventListener('DOMContentLoaded', () => {
+    const displayElement = document.getElementById('listings')
 
-  // Lodash is now imported into script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const displayListings = data => {
+        const listings = data.newsPlayer.assets
+        listings.forEach(listing => {
+            let el = document.createElement('div')
+            el.dataset.id = listing.id
+            el.innerHTML = `<h1>${listing.program_nid}</h1><p>${listing.description}</p>`
+            displayElement.appendChild(el)
+        });
+    }
 
-  return element;
-}
+    displayElement.addEventListener('click', event => {
+        alert(`You clicked on ${event.target.parentElement.dataset.id}`)
+    })
 
-document.body.appendChild(component());
+
+
+    fetch('https://tv4-graphql-prod.herokuapp.com/graphql?query={newsPlayer%20{assets%20{%20id%20program_nid%20description}}}')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            displayListings(json.data)
+        });
+})
